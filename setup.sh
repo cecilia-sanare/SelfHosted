8c4ebe3fedbb3acb1f4f770db28e66c6617f0dec
Ceci <admin@cecilias.me>
2022-09-04 22:53:41 -0500

    chore: added support for uploading custom modpacks

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 declare -A MODPACKS
 MODPACKS["ATM_7_Server_0.4.29"]="https://mediafiles.forgecdn.net/files/3949/730/Server-Files-0.4.29.zip"
-
+d
 if [ -f .env ]; then 
     export $(cat .env | xargs)
 fi
@@ -11,6 +11,7 @@ fi
 mkdir -p $BACKUP_DIR $MODPACK_DIR $SCRIPT_DIR
 cp .env $MINECRAFT_DIR
 cp -R ./scripts/* $SCRIPT_DIR
+# cp -u ./modpacks/*.zip $MODPACK_DIR
 
 cat cron.daily | crontab -
 
