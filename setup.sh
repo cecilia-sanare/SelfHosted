64ba6877e9a2fc15f7f97055d3c0d8306e00b5a1
Ceci <admin@cecilias.me>
2022-09-07 18:33:57 -0500

    emergency(origins): utilize mega.nz for modpack downloads

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 declare -A MODPACKS
 MODPACKS["ATM_7_Server_0.4.29"]="https://mediafiles.forgecdn.net/files/3949/730/Server-Files-0.4.29.zip"
+MODPACKS["TemmiesOriginsServerMods-0.1.0"]="https://mega.nz/file/gDF3TLoT#-I45q26cIHcooSbEJiq9xeVfl6VqkjtwObvYG7mgEd0"
+MODPACKS["TemmiesOriginsServerConfig-0.1.0"]="https://mega.nz/file/cfNDDLLC#xbeWULoTgk37ixT0tspjInu2RM5Dm31aJYM3PGQH3C0"
 
 if [ -f .env ]; then 
     export $(cat .env | xargs)
@@ -11,7 +13,6 @@ fi
 mkdir -p $BACKUP_DIR $MODPACK_DIR $SCRIPT_DIR
 cp -u .env $MINECRAFT_DIR
 cp -uR ./scripts/* $SCRIPT_DIR
-cp -uv ./modpacks/*.zip $MODPACK_DIR
 chown -R minecraft:docker $MODPACK_DIR
 
 cat cron.daily | crontab -
@@ -20,11 +21,14 @@ for key in ${!MODPACKS[@]}; do
     outputFile="${MODPACK_DIR}/${key}.zip"
     echo "Checking if $key exists ..."
     if [ ! -f $outputFile ]; then
-        modpackUrl=${MODPACKS[${key}]}
-        # TODO: Implment Mega Support for URLs
+        modpackUrl=$(./scripts/tools/sanitize.sh ${MODPACKS[${key}]})
         echo "Downloading modpack from: $modpackUrl ..."
-        curl $modpackUrl --output $outputFile
-        chown minecraft:docker $outputFile
+        if [[ $modpackUrl == "https://mega.nz"* ]]; then
+            (cd $MODPACK_DIR && megadl "${${MODPACK_URL/\\#/!}/file\//#!}")
+        else
+            curl $modpackUrl --output $outputFile
+            chown minecraft:docker $outputFile
+        fi
     fi
 done
 
