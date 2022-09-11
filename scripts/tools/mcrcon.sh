ac5833031659f2be721275b7f4442c8ed9476bc3
Ceci <admin@cecilias.me>
2022-09-11 01:06:09 -0500

    chore: made mcrcon docker container status safe

--- scripts/tools/mcrcon.sh
+++ scripts/tools/mcrcon.sh
@@ -5,4 +5,6 @@ SERVER=$1
 shift
 COMMAND=$@
 
-docker exec $SERVER mc-send-to-console "$@"
+if [ "$(docker ps -aq -f status=running -f name=$SERVER)" ]; then
+    docker exec $SERVER mc-send-to-console "$@"
+fi
